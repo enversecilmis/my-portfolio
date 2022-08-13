0f581e69fba6c7e330e5fe82b67e4f50649e464d
Enver <352563@ogr.ktu.edu.tr>
2022-08-13 13:52:37 +0300

    pusher half done

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+import Pusher from "pusher";
+
+
+// export const pusher = new Pusher({
+//     appId: process.env.PUSHER_APP_ID,
+// })
--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,14 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            PUSHER_APP_ID: string,
+            PUSHER_APP_KEY: string,
+            PUSHER_APP_SECRET: string,
+            PUSHER_APP_CLUSTER: string
+        }
+    }
+}

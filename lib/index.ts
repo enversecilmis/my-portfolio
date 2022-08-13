2236ec9399feccdb62d5dcfa9e3bad5027a0fee0
Enver <352563@ogr.ktu.edu.tr>
2022-08-14 02:47:46 +0300

    remove pusher stuff

--- lib/index.ts
+++
@@ -1,6 +0,0 @@
-import Pusher from "pusher";
-
-
-// export const pusher = new Pusher({
-//     appId: process.env.PUSHER_APP_ID,
-// })
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,16 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            PUSHER_APP_ID: string,
-            PUSHER_APP_KEY: string,
-            PUSHER_APP_SECRET: string,
-            PUSHER_APP_CLUSTER: string
-        }
-    }
-}

5ba0f0e7862c63a4bd9896530f7cfcfe0c8ea1cb
enversecilmis <352563@ogr.ktu.edu.tr>
2022-08-29 13:29:32 +0300

    code cleanup, redesign

--- src/components/ContactButton/ContactButton.tsx
+++ src/components/ContactButton/ContactButton.tsx
@@ -1,7 +1,7 @@
-import { MouseEventHandler, useEffect, useState } from "react";
-import { BsWhatsapp } from "react-icons/bs";
-import { FaHourglassEnd } from "react-icons/fa";
-import { IoIosCall, IoMdSend } from "react-icons/io";
+import { MouseEventHandler, useState } from "react";
+import { BsWhatsapp, BsChevronBarDown, BsChevronDown } from "react-icons/bs";
+import { BiLoaderAlt } from "react-icons/bi";
+import { IoIosCall } from "react-icons/io";
 import Modal from 'react-modal';
 import styles from "./ContactButton.module.scss";
 
@@ -11,90 +11,110 @@ import styles from "./ContactButton.module.scss";
 
 
 
-
+const gradients = [
+    styles.modalGradient1,
+    styles.modalGradient2,
+]
 
 const ContactButton: React.FC<{  }> = ({  }) => {
     const [isOpen, setIsOpen] = useState(false)
-    const [fadeClass, setFadeClass] = useState<string | undefined>("")
     const [gradClass, setGradClass] = useState(styles.themeGradient)
     const [isSending, setIsSending] = useState(false)
-
-
-    
-    useEffect(() => {
-        setFadeClass( isOpen? styles.fadeIn : styles.fadeOut )
-    }, [isOpen])
-
-
-    const gradients = [
-        styles.modalGradient1,
-        styles.modalGradient2,
-    ]
-
-
-
-    const closeModal = () => setIsOpen(false)
-    const openModal = () => setIsOpen(true)
 
 
     const submitFeedBack: MouseEventHandler<HTMLButtonElement> = (e) => {
         e.preventDefault()
         setIsSending(true)
-
         setTimeout(() => setIsSending(false), 2000)
     }
 
-
+    
     return (
         <>
             <button
                 aria-label="Contact Me"
-                className={`${styles.button} ${isOpen && styles.open}`}
-                onClick={() => openModal()}
+                className={`${styles.button} ${isOpen ? "scaleOut": "scaleIn"}`}
+                onClick={() => setIsOpen(true)}
             >
-                <IoIosCall size={25} className={`${styles.btnIcon}`}/>
+                <IoIosCall size={25}/>
             </button>
 
 
-
             <Modal
-                ariaHideApp={false}
                 isOpen={isOpen}
-                onRequestClose={closeModal}
-                className={`${styles.messageModal} ${fadeClass} ${gradClass}`}
-                overlayClassName={`${styles.messageModalOverlay}`}
-                contentLabel="Message Box"
+                onRequestClose={() => setIsOpen(false)}
+                className={`${styles.contactModal} ${gradClass} ${isOpen? "fadeIn":"fadeOut"}`}
+                overlayClassName={`${styles.contactModalOverlay}`}
+                contentLabel="İletişime Geçin"
+                ariaHideApp={false}
                 closeTimeoutMS={300}
             >
-                <p className={styles.title}>İletişime Geçin</p>
-                <form className="w-full px-10">
-                    <input className={styles.input} spellCheck={false} type="text" placeholder="İsim" />
-                    <input className={styles.input} spellCheck={false} type="email" placeholder="Email" />
-                    <textarea className={styles.input} placeholder="Mesaj..." cols={30} rows={2}></textarea>
-                    <button disabled={isSending} className={styles.sendButton} onClick={submitFeedBack} type="submit" style={isSending? { backgroundColor : "#2b912b"}: {}}>
-                        {isSending? <FaHourglassEnd /> : <IoMdSend />}
-                    </button>
-                </form>
+                <div className={styles.modalHeader}>
+                    <h5 className={styles.title}>İletişime Geçin</h5>
+                    <button onClick={() => setIsOpen(false)}><BsChevronDown/></button>
+                </div>
 
-                <div className={styles.orLine}>
-                    <div></div>
-                    <p>veya</p>
-                    <div></div>
+                <div className={styles.modelContentContainer}>
+                    <div className={styles.links}>
+                        <a href="mailto:enversecilmis1@gmail.com">enversecilmis1@gmail.com</a>
+                        <a href="tel://+905074403027">+90 507 440 3027</a>
+                        <a href="https://wa.me/905074403027" target="_blank" rel="noreferrer noopener" className={styles.wplink}>
+                            <BsWhatsapp size={50}/>
+                        </a>
+                    </div>
+
+                    <div className={styles.lineContainer}>
+                        <div className={styles.line}></div>
+                        <span className={styles.lineText}>VEYA</span>
+                        <div className={styles.line}></div>
+                    </div>
+                    
+                    <form className={styles.emailForm}>
+                        <div className={styles.inputBox}>
+                            <input
+                                id={styles.name}
+                                className={styles.input}
+                                placeholder=" "
+                                spellCheck={false}
+                                type="text"
+                                required
+                                pattern={"[A-Za-z ıİöÖüÜğĞşŞçÇ]+"}
+                            />
+                            <label htmlFor={styles.name}>isim</label>
+                        </div>
+
+                        <div className={styles.inputBox}>
+                            <input
+                                id={styles.email}
+                                className={styles.input}
+                                placeholder=" "
+                                spellCheck={false}
+                                type="email"
+                                required
+                            />
+                            <label htmlFor={styles.email}>email</label>
+                        </div>
+
+                        <textarea
+                            className={styles.input}
+                            placeholder="Mesaj..."
+                            spellCheck={false}
+                            required
+                            rows={3}>
+                        </textarea>
+                        <button
+                            disabled={isSending}
+                            className={styles.sendButton}
+                            onClick={submitFeedBack}
+                            type="submit"
+                        >
+                            {isSending? <BiLoaderAlt size={25} className="animate-spin"/> : "GÖNDER"}
+                        </button>
+                    </form>
                 </div>
                 
-                <div className="flex flex-col items-center mt-10">
-                    <button
-                        className="mb-5"
-                        onClick={() => window.open('mailto:enversecilmis1@gmail.com')}>
-                            enversecilmis1@gmail.com
-                        </button>
-                    <a className="mb-5" href="tel://+905074403027">+90 507 440 3027</a>
-                    <a href="https://wa.me/905074403027" target="_blank" rel="noreferrer noopener" className={styles.wplink}>
-                        <BsWhatsapp size={50}/>
-                    </a>
-                </div>
 
-                <div className="absolute bottom-0 right-0 px-4 pb-2">
+                <div className={styles.gradientButtons}>
                     {gradients.map((g,idx) => 
                         <button
                             key={idx}

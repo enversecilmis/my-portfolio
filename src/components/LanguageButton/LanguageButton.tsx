8ba2377dcfc7bc2f0889de0cd26987c366b73089
enversecilmis <enversecilmis1@gmail.com>
2022-09-08 23:48:03 +0300

    language selection component

---
+++ src/components/LanguageButton/LanguageButton.tsx
@@ -0,0 +1,38 @@
+import styles from './LanguageButton.module.scss'
+import { IoLanguage, IoChevronDown } from "react-icons/io5";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+
+
+
+
+
+const LanguageButton: React.FC<{ className?: string }> = ({ className }) => {
+
+	const { asPath, locales, locale } = useRouter()
+
+	return (
+		<div  className={`${styles.container} ${className}`}>
+			<IoLanguage />
+			<IoChevronDown />
+
+			<div className={styles.languagesContainer}>
+				{locales?.map((val) => (
+					<Link href={asPath} locale={val}>
+						<a className={`${styles.localeOption} ${val === locale ? styles.currentLocale:""}`}>
+							{val}
+						</a>
+					</Link>
+				))}
+			</div>
+		</div>
+	)
+}
+
+
+
+
+
+
+export default LanguageButton

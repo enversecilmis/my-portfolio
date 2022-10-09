f77958b6f1c3fb1de31a147406f922757f7db315
Enver Seçilmiş <enversecilmis1@gmail.com>
2022-10-09 13:34:27 +0300

    static props for translations

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import styles from "../../styles/projects.module.scss";
-import Link from "next/link";
-import { HiHome } from "react-icons/hi";
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
+import HeaderNav from "../../components/HeaderNav/HeaderNav";
+import ContactButton from "../../components/ContactButton/ContactButton";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 
 
@@ -16,28 +17,32 @@ const Projects: NextPage = () => {
         <div>
             <Head>
                 <title>Projects</title>
+                <link rel="icon" href="/favicon.ico" />
             </Head>
 
+            <HeaderNav/>
+            <ContactButton/>
+            
+            <div className={styles.container}>
+              
 
-            <div className={`${styles.container} w-full h-screen`}>
-                <Link href="/">
-                    <a className="inline-block border-2 rounded-full box-content text-gray-300 border-gray-300 hover:text-gray-50 hover:border-gray-50 p-1 m-2 transition-colors duration-300">
-                        <HiHome size={30} />
-                    </a>
-                </Link>
 
-                <div className="flex flex-col items-center text-gray-100 md:text-5xl sm:text-3xl text-3xl font-mono shadow-md pb-7">
-                    <h1>My Projects</h1>
-                    <h2 className="mt-6 sm:text-2xl text-xl">Coming Soon...</h2>
-                </div>
+
+
             </div>
-
-
         </div>
     )
 }
 
 
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+    return {
+      props: {
+        ...(await serverSideTranslations(locale as string, ["common", "projects", "header", "notifications"])),
+      }
+    }
+  }
 
 
 

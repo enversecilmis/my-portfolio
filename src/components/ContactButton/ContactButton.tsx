df3a42b4428ea05b03ec2b3fd0e4d9b8bbd08de5
Enver Seçilmiş <enversecilmis1@gmail.com>
2022-10-13 21:56:20 +0300

    translations for contact modal

--- src/components/ContactButton/ContactButton.tsx
+++ src/components/ContactButton/ContactButton.tsx
@@ -4,19 +4,16 @@ import { BiLoaderAlt } from "react-icons/bi";
 import { IoIosCall } from "react-icons/io";
 import Modal from 'react-modal';
 import styles from "./ContactButton.module.scss";
+import { useTranslation } from "next-i18next";
 
 
 
 
 
 
-
-const gradients = [
-    styles.modalGradient1,
-    styles.modalGradient2,
-]
 
 const ContactButton: React.FC<{  }> = ({  }) => {
+    const { t } = useTranslation("contact-button")
     const [isOpen, setIsOpen] = useState(false)
     const [gradClass, setGradClass] = useState(styles.themeGradient)
     const [isSending, setIsSending] = useState(false)
@@ -32,7 +29,8 @@ const ContactButton: React.FC<{  }> = ({  }) => {
     return (
         <>
             <button
-                aria-label="Contact Me"
+                title={t("contact")}
+                aria-label={t("contact")}
                 className={`${styles.button} ${isOpen ? "scaleOut": "scaleIn"}`}
                 onClick={() => setIsOpen(true)}
             >
@@ -45,12 +43,12 @@ const ContactButton: React.FC<{  }> = ({  }) => {
                 onRequestClose={() => setIsOpen(false)}
                 className={`${styles.contactModal} ${gradClass} ${isOpen? "fadeIn":"fadeOut"}`}
                 overlayClassName={`${styles.contactModalOverlay}`}
-                contentLabel="İletişime Geçin"
+                contentLabel={t("contact")}
                 ariaHideApp={false}
                 closeTimeoutMS={300}
             >
                 <div className={styles.modalHeader}>
-                    <h5 className={styles.title}>İletişime Geçin</h5>
+                    <h5 className={styles.title}>{t("contact")}</h5>
                     <button onClick={() => setIsOpen(false)}><BsChevronDown/></button>
                 </div>
 
@@ -65,7 +63,7 @@ const ContactButton: React.FC<{  }> = ({  }) => {
 
                     <div className={styles.lineContainer}>
                         <div className={styles.line}></div>
-                        <span className={styles.lineText}>VEYA</span>
+                        <span className={styles.lineText}>{t("or")}</span>
                         <div className={styles.line}></div>
                     </div>
                     
@@ -80,7 +78,7 @@ const ContactButton: React.FC<{  }> = ({  }) => {
                                 required
                                 pattern={"[A-Za-z ıİöÖüÜğĞşŞçÇ]+"}
                             />
-                            <label htmlFor={styles.name}>isim</label>
+                            <label htmlFor={styles.name}>*{t("name")}</label>
                         </div>
 
                         <div className={styles.inputBox}>
@@ -92,12 +90,12 @@ const ContactButton: React.FC<{  }> = ({  }) => {
                                 type="email"
                                 required
                             />
-                            <label htmlFor={styles.email}>email</label>
+                            <label htmlFor={styles.email}>*email</label>
                         </div>
 
                         <textarea
                             className={styles.input}
-                            placeholder="Mesaj..."
+                            placeholder={`*${t("message")}`}
                             spellCheck={false}
                             required
                             rows={3}>
@@ -108,20 +106,13 @@ const ContactButton: React.FC<{  }> = ({  }) => {
                             onClick={submitFeedBack}
                             type="submit"
                         >
-                            {isSending? <BiLoaderAlt size={25} className="animate-spin"/> : "GÖNDER"}
+                            {isSending? <BiLoaderAlt size={25} className="animate-spin"/> : t("send")}
                         </button>
                     </form>
                 </div>
                 
 
                 <div className={styles.gradientButtons}>
-                    {gradients.map((g,idx) => 
-                        <button
-                            key={idx}
-                            onClick={() => setGradClass(g)}
-                            className={`${gradClass === g? g+" shadow-md scale-110" : g+" shadow-sm"} transition-all duration-300 w-6 h-6 rounded-full shadow-black mx-2`}
-                        ></button>
-                    )}
                     <button
                         onClick={() => setGradClass(styles.themeGradient)}
                         className={`${styles.themeGradButton} ${gradClass === styles.themeGradient? "shadow-md scale-110 rotate-45" : "shadow-sm -rotate-45"} transition-all duration-300 w-6 h-6 rounded-full shadow-black mx-2`}

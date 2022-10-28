1344da2a9c396e308ff3569b5d6e6ccda4af66cd
Enver Seçilmiş <enversecilmis1@gmail.com>
2022-10-28 19:11:13 +0300

    fixed animation bug

--- src/components/MultistepInput/MultistepInput.tsx
+++ src/components/MultistepInput/MultistepInput.tsx
@@ -1,53 +1,100 @@
-import { Children, FormEventHandler, ReactNode, useState } from 'react'
+import { Children, FormEventHandler, ReactNode, useRef, useState } from 'react'
 import ThemedButton from '../ThemedButton/ThemedButton'
 import styles from './MultistepInput.module.scss'
 import { AnimatePresence, motion } from "framer-motion"
 
 
-type Props = {
-    onFinish: () => void
-    children?: ReactNode | ReactNode[]
+
+const variants = {
+    center: {
+        x: 0,
+        opacity: 1
+    },
+    enter: (direction: number) => ({
+        x: direction*100,
+        opacity: 0.5
+    }),
+    exit: (direction: number) => ({
+        x: direction*-100,
+        opacity: 0,
+        zIndex: 0
+    })
 }
 
+
+type Props = {
+    className?: string
+    onFinish?: () => void
+    children?: ReactNode | ReactNode[]
+}
 /**
  * Every react children becomes a step.
+ * And every child needs a key.
  */
-const MultistepInput: React.FC<Props> = ({ 
-    onFinish,
+const MultistepInput: React.FC<Props> = ({
+    onFinish = () => {},
     children,
- }) => {
+    className
+}) => {
     const [stepIndex, setStepIndex] = useState(0)
+    const [direction, setDirection] = useState(0)
+
     const childrenArray = Children.toArray(children)
-    const lastIndex = childrenArray.length-1
+    const lastIndex = childrenArray.length - 1
+
 
 
     const nextOrFinish: FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault()
-        stepIndex === lastIndex?
-            onFinish():
+        stepIndex === lastIndex ?
+            onFinish() :
             next()
     }
 
-    const next = () => setStepIndex(p => p >= lastIndex? lastIndex:p+1)
-    const previous = () => setStepIndex(p => p === 0? 0:p-1)
-    
+    const next = () => {
+        setStepIndex(p => p >= lastIndex ? lastIndex : p + 1)
+        setDirection(1)
+    }
+    const previous = () => {
+        setStepIndex(p => p === 0 ? 0 : p - 1)
+        setDirection(-1)
+    }
+
+
     return (
         <form
-            className={styles.container}
+            className={`${styles.container} ${className}`}
             onSubmit={nextOrFinish}
         >
-            <div className={styles.stepTag}>{stepIndex+1}/{lastIndex+1}</div>
-            {childrenArray[stepIndex]}
+            <div className={styles.stepTag}>{stepIndex + 1}/{lastIndex + 1}</div>
+            <AnimatePresence initial={false} custom={direction} mode="popLayout">
+                <motion.div
+                    key={stepIndex}
+                    custom={direction}
+                    className={styles.stepContainer}
+                    variants={variants}
+                    transition={{
+                        opacity: { type: "keyframes", duration: .3 },
+                        x: { type: "keyframes", ease: "easeOut", duration: .3 }
+                    }}
+                    initial="enter"
+                    animate="center"
+                    exit="exit"
+                >
+                    {childrenArray[stepIndex]}
+                </motion.div>
+            </AnimatePresence>
             <div className={styles.buttons}>
                 <ThemedButton
                     type='button'
                     onClick={previous}
                     label="Previous"
                     className={styles.button}
+                    disabled={stepIndex===0}
                 />
                 <ThemedButton
                     type='submit'
-                    label={stepIndex === lastIndex? "Finish":"Next"}
+                    label={stepIndex === lastIndex ? "Finish" : "Next"}
                     className={styles.button}
                 />
             </div>

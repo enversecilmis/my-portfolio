fb4b4116c1b39e47ab7a6a3e695a46d65923f98d
Enver Seçilmiş <enversecilmis1@gmail.com>
2022-12-20 20:18:11 +0300

    fixed the overflow bug

--- src/components/atoms/HoverHelp/HoverHelp.tsx
+++ src/components/atoms/HoverHelp/HoverHelp.tsx
@@ -1,5 +1,5 @@
 import { BiHelpCircle } from "react-icons/bi"
-import { autoPlacement, useFloating } from "@floating-ui/react-dom"
+import { autoPlacement, autoUpdate, shift, useFloating } from "@floating-ui/react-dom"
 
 import styles from "./HoverHelp.module.scss"
 
@@ -7,18 +7,23 @@ import styles from "./HoverHelp.module.scss"
 
 type Props = {
     message: string
-    calssName?: string
+    className?: string
 }
 
 const HoverHelp: React.FC<Props> = ({
 	message,
-	calssName,
+	className,
 }) => {
-	const { x, y, reference, floating, strategy } = useFloating({ middleware: [autoPlacement()]})
+	const { x, y, reference, floating, update } = useFloating({
+		middleware: [autoPlacement(), shift()],
+	})
 
 
 	return (
-		<div className={`${styles.container} ${calssName}`}>
+		<div
+			className={styles.container}
+			onMouseEnter={update}
+		>
 			<div
 				ref={reference}
 				className={styles.icon}
@@ -27,9 +32,8 @@ const HoverHelp: React.FC<Props> = ({
 			</div>
 			<p
 				ref={floating}
-				className={styles.message}
+				className={`${styles.message} ${className}`}
 				style={{
-					position: strategy,
 					top: y ?? 0,
 					left: x ?? 0,
 				}}

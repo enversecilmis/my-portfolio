1f5ee6d7753e00a45c0b923d6e31f27efcb7437f
Enver Seçilmiş <enversecilmis1@gmail.com>
2022-12-22 13:01:59 +0300

    useCallBack

--- src/contexts/NotificationContext.tsx
+++ src/contexts/NotificationContext.tsx
@@ -1,10 +1,11 @@
-import { createContext, ReactNode, useContext, useRef, useState } from "react"
+import { createContext, ReactNode, useCallback, useContext, useRef, useState } from "react"
 
+// TODO: Put useNotification in hooks as well as useThemePref.
 
 type NotificationType = "error" | "warning" | "info"
 type Notification = { key: number, type: NotificationType, message: string, source?: string}
 type PushNotificationOptions = {
-    durationSeconds?: number
+    duration?: number
     type?: NotificationType
     source?: string
 }
@@ -26,25 +27,25 @@ const NotificationProvider: React.FC<{children?: ReactNode}> = ({ children }) =>
 	const [notifications, setNotifications] = useState<Notification[]>([])
 	const itemKey = useRef(0)
 
-	const pushNotification: PushNotificationFunc = (message, options) => {
+
+	const pushNotification = useCallback<PushNotificationFunc>((message, options) => {
 		const key = itemKey.current
 		const type = options?.type || "info"
 		const source = options?.source
 
 		setNotifications(p => [...p, { key, type, message, source }])
 
-		if (options?.durationSeconds) {
+		if (options?.duration) {
 			setTimeout(() => {
 				setNotifications(p => p.filter(val => val.key !== key))
-			}, options.durationSeconds)
+			}, options.duration)
 		}
 		itemKey.current += 1
 		return key
-	}
+	}, [])
 
-	const deleteNotification: DeleteNotificationFunc = (key) => {
-		setNotifications(p => p.filter(val => val.key !== key))
-	}
+
+	const deleteNotification = useCallback<DeleteNotificationFunc>(key => setNotifications(p => p.filter(val => val.key !== key)), [])
 
 
 	return (
@@ -65,6 +66,7 @@ const useNotification = () => {
 
 
 export {
+	NotificationContext,
 	NotificationProvider,
 	useNotification,
 }

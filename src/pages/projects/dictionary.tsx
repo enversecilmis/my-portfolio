5e54bb4424cb5d6cb2acf6aedb003841e9aea2c5
Enver Seçilmiş <enversecilmis1@gmail.com>
2022-12-22 13:00:13 +0300

    readibility & atomic creation

--- src/pages/projects/dictionary.tsx
+++ src/pages/projects/dictionary.tsx
@@ -1,8 +1,9 @@
-import { ReactElement, useEffect, useRef, useState } from "react"
+import { ReactElement, useRef, useState } from "react"
 import BasicStats from "@components/atoms/BasicStats/BasicStats"
 import BoxSection from "@components/atoms/BoxSection/BoxSection"
 import ContentSection from "@components/atoms/ContentSection/ContentSection"
 import Histogram from "@components/molecules/Histogram/Histogram"
+import getErrorMessage from "@utils/get-error-message"
 import { GetStaticProps } from "next"
 import Head from "next/head"
 import { useTranslation } from "next-i18next"
@@ -20,6 +21,7 @@ import GetTableSize from "../../components/organisms/GetTableSize/GetTableSize"
 import { useNotification } from "../../contexts/NotificationContext"
 import {
 	DictionaryTypeError,
+	ReturnTypeError,
 	TableSizeError,
 } from "../../projects-src/hashtabledict/errors"
 import { ArrayDictionary, HashTableDictionary } from "../../projects-src/hashtabledict/hashtabledict"
@@ -27,6 +29,8 @@ import {
 	CollisionHandler,
 	HashStringFunction,
 } from "../../projects-src/hashtabledict/types"
+import evalCollisionHandler from "../../projects-src/hashtabledict/utils/eval-collision-handler"
+import evalHashFunction from "../../projects-src/hashtabledict/utils/eval-hash-function"
 import runCreateTableArrWorker from "../../projects-src/hashtabledict/utils/run-create-table-arr-worker"
 import { NextPageWithLayout } from "../_app"
 
@@ -51,115 +55,98 @@ const Dictionary: NextPageWithLayout = () => {
 	const [collisionHandlerString, setCollisionHandlerString] = useState("")
 	const [throwCollisionLoopError, setCollisionLoopError] = useState(false)
 
+	const hashFunction = useRef<HashStringFunction>()
+	const collisionHandler = useRef<CollisionHandler>()
+	const resetKey = useRef(0)
+
 	const { t: dictionaryT } = useTranslation("dictionary")
 	const { t: commonT } = useTranslation("common")
 	const { pushNotification } = useNotification()
 	const toScrollElement = useRef<HTMLDivElement>(null)
-	const resetKey = useRef(0)
-
-
-	useEffect(() => {
-		if (!toScrollElement.current)
-			return
-		toScrollElement.current.scrollIntoView({ behavior: "smooth" })
-	}, [hashDict])
 
 
 
-	const validateInputsAndCreateHashTable = async () => {
-		let hashFunction: HashStringFunction | undefined
-		let collisionHandler: CollisionHandler | undefined
-
-		// Validate hash function.
+	const validateHashFunction = () => {
 		try {
-			const hashFunctionEval = eval(`(input) => {${hashFunctionString}}`)
-			const hashFuncOutput = hashFunctionEval("helo")
-
-			if (typeof hashFuncOutput !== "number")
-				throw new Error(dictionaryT("returnTypeNumberError"))
-
-			hashFunction = hashFunctionEval
-		} catch (error) {
-			hashFunction = undefined
-			if (error instanceof Error) {
-				pushNotification(error.toString(), {
-					type: "error",
-					source: dictionaryT("hashFuncInput"),
-				})
-			}
+			hashFunction.current = evalHashFunction(`(input) => {${hashFunctionString}}`)
+			return true
 		}
+		catch (error) {
+			hashFunction.current = undefined
+			const message = getErrorMessage(error, [
+				[ReturnTypeError, `${dictionaryT("returnTypeNumberError")}`],
+				["default", "Unknown Error"],
+			])
 
-		// Validate collision handler.
+			pushNotification(message, {
+				type: "error",
+				source: dictionaryT("hashFuncInput"),
+				duration: 6000,
+			})
+			return false
+		}
+	}
+
+
+	const validateCollisionHandler = () => {
 		try {
-			const collisionHandlerEval = eval(
+			collisionHandler.current = evalCollisionHandler(
 				`(currentHashValue, input, iteration) => {${collisionHandlerString}}`,
 			)
-			const collisionFuncOutput = collisionHandlerEval(19, "helo", 1)
-
-			if (typeof collisionFuncOutput !== "number")
-				throw new Error(dictionaryT("returnTypeNumberError"))
-
-			collisionHandler = collisionHandlerEval
-		} catch (error) {
-			collisionHandler = undefined
-			if (error instanceof Error) {
-				pushNotification(error.toString(), {
-					type: "error",
-					source: dictionaryT("collisionHandlerInput"),
-				})
-			}
+			return true
 		}
+		catch (error) {
+			collisionHandler.current = undefined
+			const message = getErrorMessage(error, [
+				[ReturnTypeError, `${dictionaryT("returnTypeNumberError")}`],
+				["default", "Unknown Error"],
+			])
 
-		// Try to create hash table dictionary.
+			pushNotification(message, {
+				type: "error",
+				source: dictionaryT("collisionHandlerInput"),
+			})
+			return false
+		}
+	}
+
+	const createHashTable = async () => {
 		try {
-			if (hashFunction === undefined || collisionHandler === undefined || arrDict === undefined)
+			if (hashFunction.current === undefined ||
+				collisionHandler.current === undefined ||
+				arrDict === undefined)
 				return
 
+			// Create the table in worker
 			const { tableArray, allCollisions } = await runCreateTableArrWorker(arrDict.dictArray, {
-				hashFunction,
-				collisionHandler,
+				hashFunction: hashFunction.current,
+				collisionHandler: collisionHandler.current,
 				tableSize,
 				throwCollisionLoopError,
 			})
 
 			const hashTableDict = new HashTableDictionary(tableArray, {
-				hashFunction,
-				collisionHandler,
+				hashFunction: hashFunction.current,
+				collisionHandler: collisionHandler.current,
 				allCollisions,
 				throwCollisionLoopError,
 			})
 
-
 			setHashDict(hashTableDict)
+			toScrollElement.current?.scrollIntoView({ behavior: "smooth" })
 			resetKey.current++
-		} catch (error) {
-			if (error instanceof DictionaryTypeError) {
-				const message = dictionaryT("createDictArrError")
+		}
+		catch (error) {
+			const message = getErrorMessage(error, [
+				[DictionaryTypeError, `${dictionaryT("createDictArrError")}`],
+				[TableSizeError, (e) => `${e.message}\r\n${dictionaryT("hashTableSizeError")}`],
+			])
 
-				pushNotification(message, {
-					type: "error",
-					source: dictionaryT("createDictArr"),
-					durationSeconds: 6000,
-				})
-				return
-			}
-			if (error instanceof TableSizeError) {
-				const message = `${error.message}\r\n${dictionaryT("hashTableSizeError")}`
-
-				pushNotification(message, {
-					type: "error",
-					source: dictionaryT("createDictArr"),
-					durationSeconds: 6000,
-				})
-				return
-			}
-			if (error instanceof Error) {
-				pushNotification(error.toString(), {
-					type: "error",
-					source: dictionaryT("createDictArr"),
-					durationSeconds: 6000,
-				})
-			}
+			pushNotification(message, {
+				type: "error",
+				source: dictionaryT("createDictArr"),
+				duration: 6000,
+			})
 		}
 	}
 
@@ -180,67 +167,78 @@ const Dictionary: NextPageWithLayout = () => {
 
 				<MultistepForm
 					className={styles.multiStepForm}
-					onFinish={validateInputsAndCreateHashTable}
-				>
+					onFinish={createHashTable}>
 					<MultistepForm.Item disableNext={!arrDict}>
 						<CreateDictionaryArray
 							fileContentState={[fileContent, setFileContent]}
 							wordSeperatorState={[wordSeperator, setWordSeperator]}
 							pairSeperatorState={[pairSeperator, setPairSeperator]}
-							arrDictState={[arrDict, setArrDict]}
-						/>
+							arrDictState={[arrDict, setArrDict]}/>
 					</MultistepForm.Item>
-					<GetHashFunction
-						hashFunctionStringState={[hashFunctionString, setHashFunctionString]}
-					/>
-					<GetCollisionHandler
-						collisionHandlerStringState={[collisionHandlerString, setCollisionHandlerString]}
-						throwCollisionLoopErrorState={[throwCollisionLoopError, setCollisionLoopError]}
-					/>
-					<GetTableSize
-						hashTableSizeState={[tableSize, setTableSize]}
-						arrDict={arrDict}
-					/>
+
+					<MultistepForm.Item
+						onNext={validateHashFunction}>
+						<GetHashFunction
+							hashFunctionStringState={[hashFunctionString, setHashFunctionString]}/>
+					</MultistepForm.Item>
+
+					<MultistepForm.Item
+						onNext={validateCollisionHandler}>
+						<GetCollisionHandler
+							collisionHandlerStringState={[collisionHandlerString, setCollisionHandlerString]}
+							throwCollisionLoopErrorState={[throwCollisionLoopError, setCollisionLoopError]}/>
+					</MultistepForm.Item>
+
+					<MultistepForm.Item>
+						<GetTableSize
+							hashTableSizeState={[tableSize, setTableSize]}
+							arrDict={arrDict}/>
+					</MultistepForm.Item>
 				</MultistepForm>
 
 
-				{hashDict && arrDict &&
 				<div
 					key={resetKey.current}
 					ref={toScrollElement}
-					className={styles.tableReadySection}
-				>
-					<BoxSection className={styles.searchSection} heading={dictionaryT("searchInDictionaries")} hTag="h2">
-						<DictionarySearch
-							hashDict={hashDict}
-							arrDict={arrDict}
-						/>
-					</BoxSection>
+					className={styles.tableReadySection}>
+					{hashDict && arrDict &&
+					<>
+						<BoxSection
+							className={styles.searchSection}
+							heading={dictionaryT("searchInDictionaries")} hTag="h2">
+							<DictionarySearch
+								hashDict={hashDict}
+								arrDict={arrDict}/>
+						</BoxSection>
 
-					<BoxSection className={styles.statSection} heading={dictionaryT("collisionStats")} hTag="h2">
-						<BasicStats
-							title={"Temel İstatistikler"}
-							array={hashDict.allCollisions}
-							postfix={commonT("collision")}
-							fractionDigits={3}
-						/>
-						<Histogram
-							title={dictionaryT("collisionsHistogram")}
-							array={hashDict.allCollisions}
-							tagX={dictionaryT("collisionsTag")}
-							tagY={dictionaryT("occurenceTag")}
-							fractionDigits={3}
-						/>
-					</BoxSection>
+						<BoxSection
+							className={styles.statSection}
+							heading={dictionaryT("collisionStats")}
+							hTag="h2">
+							<BasicStats
+								title={"Temel İstatistikler"}
+								array={hashDict.allCollisions}
+								postfix={commonT("collision")}
+								fractionDigits={3}/>
+							<Histogram
+								title={dictionaryT("collisionsHistogram")}
+								array={hashDict.allCollisions}
+								tagX={dictionaryT("collisionsTag")}
+								tagY={dictionaryT("occurenceTag")}
+								fractionDigits={3}/>
+						</BoxSection>
 
-					<BoxSection className={styles.searchSection} hTag="h2" heading={dictionaryT("comparison")}>
-						<CompareDictionaries
-							arrDict={arrDict}
-							hashDict={hashDict}
-						/>
-					</BoxSection>
+						<BoxSection
+							className={styles.searchSection}
+							heading={dictionaryT("comparison")}
+							hTag="h2">
+							<CompareDictionaries
+								arrDict={arrDict}
+								hashDict={hashDict}/>
+						</BoxSection>
+					</>
+					}
 				</div>
-				}
 			</ContentSection>
 		</>
 	)

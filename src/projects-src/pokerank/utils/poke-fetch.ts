3cf31b66336c71ad30f2b1ddae0461d4a69d0b49
Enver Seçilmiş <enversecilmis1@gmail.com>
2023-01-06 01:52:32 +0300

    falvor text

--- src/projects-src/pokerank/utils/poke-fetch.ts
+++ src/projects-src/pokerank/utils/poke-fetch.ts
@@ -1,14 +1,19 @@
 import { Poke } from "../types"
 
 
+const imgUrlBase = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
+const fetchUrlBase = "https://pokeapi.co/api/v2/pokemon-species"
+
 
 const pokeFetch = async (pokeId: number) => {
-	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+	// const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+	const res = await fetch(`${fetchUrlBase}/${pokeId}`)
 	const pokemon = await res.json()
 
 	const poke = {
 		name: pokemon.name,
-		image: pokemon.sprites.front_default,
+		image: `${imgUrlBase}/${pokeId}.png`,
+		text: pokemon.flavor_text_entries[0].flavor_text,
 	}
 
 	return poke as Poke
